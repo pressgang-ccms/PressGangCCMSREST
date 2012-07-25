da7d6590065696ecdbb8677d0537b55bcd013517
Matthew Casperson <mcaspers@redhat.com>
2012-07-25 14:59:37 +1000

    .

--- src/main/java/org/jboss/pressgangccms/restserver/REST.java
+++ src/main/java/org/jboss/pressgangccms/restserver/REST.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-/** A test REST interface. */
+/** A test REST interface */
 @Path("/message")
 public class REST
 {
