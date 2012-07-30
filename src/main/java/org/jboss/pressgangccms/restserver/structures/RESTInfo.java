dac73a6b077c2f16a46068a2467b37d9da46a914
Matthew Casperson <mcaspers@redhat.com>
2012-07-30 10:28:42 +1000

    Added REST version info endpoint

---
+++ src/main/java/org/jboss/pressgangccms/restserver/structures/RESTInfo.java
@@ -0,0 +1,10 @@
+package org.jboss.pressgangccms.restserver.structures;
+
+import java.util.List;
+
+/** Provides information on all the versions of the REST interface */
+public class RESTInfo
+{
+	public Float latest;
+	public List<RESTVersionInfo> versions;
+}
