a22de0032432d6b768d556caa526e80892818e38
Matthew Casperson <mcaspers@redhat.com>
2012-08-01 08:23:35 +1000

    Working on tests

--- src/test/resources/org/jboss/pressgangccms/test/IntegrationTests.java
+++ src/test/resources/org/jboss/pressgangccms/test/IntegrationTests.java
@@ -3,6 +3,9 @@ package org.jboss.pressgangccms.test;
 import java.util.Map;
 
 import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+
 import static com.jayway.restassured.RestAssured.*;
 import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*; 
@@ -12,22 +15,36 @@ public class IntegrationTests
 	private static final String RESTPASS = "REST_PASSWORD";
 	private static final String RESTUSER = "REST_USERNAME";
 	
-	@Test
+	//@Test
 	public void getRESTInfo()
 	{
+		RestAssured.reset();
+		
 		expect().statusCode(200).when().get("http://devrest-pressgangccms.rhcloud.com");
 	}
 	
 	@Test
 	public void getTags()
 	{
+		final String path = "/1/tags/get/json/all";
+		
 		final Map<String, String> env = System.getenv();
 		if (env.containsKey(RESTPASS) && env.containsKey(RESTUSER))
 		{
 			final String restUser = env.get(RESTUSER);
 			final String restPass = env.get(RESTPASS);
 			
-			given().auth().basic(restUser, restPass).expect().statusCode(200).when().get("http://devrest-pressgangccms.rhcloud.com/1/tags/get/json/all?expand=%7B%22branches%22%3A%5B%7B%22trunk%22%3A%7B%22name%22%3A%20%22tags%22%2C%22showSize%22%3Atrue%7D%7D%5D%7D");
+			RestAssured.reset();
+			RestAssured.baseURI = "http://devrest-pressgangccms.rhcloud.com";
+			RestAssured.authentication = basic(restUser, restPass);
+			RestAssured.urlEncodingEnabled = true;
+			
+			given()
+				.param("expand", "{\"branches\":[{\"trunk\":{\"name\":\"tags\",\"showSize\":true}}]}")
+			.expect()
+				.statusCode(200)
+			.when()
+				.get(path);
 		}
 	}
 }
