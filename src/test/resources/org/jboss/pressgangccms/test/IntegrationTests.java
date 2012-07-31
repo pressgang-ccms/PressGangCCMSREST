a66475339eb11a6f12622f756e42ae7cd9d0603a
Matthew Casperson <mcaspers@redhat.com>
2012-08-01 09:07:50 +1000

    Still working on tests

--- src/test/resources/org/jboss/pressgangccms/test/IntegrationTests.java
+++ src/test/resources/org/jboss/pressgangccms/test/IntegrationTests.java
@@ -1,10 +1,14 @@
 package org.jboss.pressgangccms.test;
 
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.response.Response;
 
 import static com.jayway.restassured.RestAssured.*;
 import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
@@ -40,12 +44,61 @@ public class IntegrationTests
 			RestAssured.urlEncodingEnabled = true;
 			RestAssured.port = 80;
 			
-			given()
-				.param("expand", "{\"branches\":[{\"trunk\":{\"name\":\"tags\",\"showSize\":true}}]}")
-			.expect()
-				.statusCode(200)
-			.when()
-				.get(path);
+			/* Test the response */
+			final Response res = get(path);
+			assertEquals(200, res.getStatusCode());
+			
+			final String json = res.asString();
+			final JsonPath jp = new JsonPath(json);
+						
+			assertEquals(jp.getInt("size"), 0);
+			assertNull(jp.get("endExpandIndex"));
+			assertNull(jp.get("startExpandIndex"));
+			assertNull(jp.get("items"));
+			assertTrue("tags".equals(jp.getString("expand")));
+		}
+	}
+	
+	@Test
+	public void getTagsExpanded()
+	{
+		final String path = "/1/tags/get/json/all";
+		
+		final Map<String, String> env = System.getenv();
+		if (env.containsKey(RESTPASS) && env.containsKey(RESTUSER))
+		{
+			final String restUser = env.get(RESTUSER);
+			final String restPass = env.get(RESTPASS);
+			
+			RestAssured.reset();
+			RestAssured.baseURI = "http://devrest-pressgangccms.rhcloud.com";
+			RestAssured.authentication = basic(restUser, restPass);
+			RestAssured.urlEncodingEnabled = true;
+			RestAssured.port = 80;
+			
+			/* Test the response */
+			final Response res = given().param("expand", "{\"branches\":[{\"trunk\":{\"name\":\"tags\",\"showSize\":true}}]}").get(path);
+			assertEquals(200, res.getStatusCode());
+			
+			final String json = res.asString();
+			final JsonPath jp = new JsonPath(json);
+						
+			assertEquals(jp.getInt("endExpandIndex"), jp.getInt("size"));
+			assertEquals(jp.getInt("startExpandIndex"), 0);
+			assertTrue("tags".equals(jp.getString("expand")));
+			
+			final List<Map> items = jp.getList("items", Map.class);
+			
+			assertNotNull(items);			
+			assertTrue(items.size() == jp.getInt("size"));
+			
+			for (final Map item : items)
+			{
+				assertNotNull(item.get("id"));
+				assertTrue(item.get("id").toString().matches("\\d+"));
+				assertNull(item.get("revision"));
+				assertNull(item.get("configuredParameters"));
+			}
 		}
 	}
 }
