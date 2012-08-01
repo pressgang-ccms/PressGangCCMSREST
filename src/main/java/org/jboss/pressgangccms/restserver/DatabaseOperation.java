df8dd481266c9862f951ae94c63d9a607b7d9765
Matthew Casperson <mcaspers@redhat.com>
2012-08-01 13:03:43 +1000

    Added some comments

--- src/main/java/org/jboss/pressgangccms/restserver/DatabaseOperation.java
+++ src/main/java/org/jboss/pressgangccms/restserver/DatabaseOperation.java
@@ -1,5 +1,10 @@
 package org.jboss.pressgangccms.restserver;
 
+/**
+ * Defines the type of action that will be taken against the database
+ * @author Matthew Casperson
+ *
+ */
 public enum DatabaseOperation
 {
 	CREATE,
--- src/main/java/org/jboss/pressgangccms/restserver/RESTSecurity.java
+++ src/main/java/org/jboss/pressgangccms/restserver/RESTSecurity.java
@@ -1,6 +1,12 @@
 package org.jboss.pressgangccms.restserver;
 
+/**
+ * Defines the role that has access to the REST interface
+ * @author Matthew Casperson
+ *
+ */
 public final class RESTSecurity
 {
+	/** The role that can access the REST interface */
 	public static final String RESTv1ROLE = "RESTAdmin";
 }
