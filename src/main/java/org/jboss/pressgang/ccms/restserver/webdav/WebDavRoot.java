ba576a07b2ebd85ac2de1fffe11a56576dd042bb
Matthew Casperson <matthewcasperson@gmail.com>
2013-05-06 08:37:36 +1000

    Working on getting basic webdav functionality imported

--- src/main/java/org/jboss/pressgang/ccms/restserver/webdav/WebDavRoot.java
+++ src/main/java/org/jboss/pressgang/ccms/restserver/webdav/WebDavRoot.java
@@ -1,6 +1,7 @@
 package org.jboss.pressgang.ccms.restserver.webdav;
 
 import net.java.dev.webdav.jaxrs.methods.PROPFIND;
+import static net.java.dev.webdav.jaxrs.xml.properties.ResourceType.COLLECTION;
 import net.java.dev.webdav.jaxrs.xml.elements.*;
 import net.java.dev.webdav.jaxrs.xml.elements.Response;
 import net.java.dev.webdav.jaxrs.xml.properties.*;
@@ -29,8 +30,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.ext.Providers;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -38,6 +41,9 @@ import java.util.List;
  */
 @Path("webdav")
 public class WebDavRoot implements WebDavResource {
+    private static final String XML_MIME = "application/xml;charset=UTF-8";
+    private static final String OCTET_STREAM_MIME = "application/octet-stream";
+
     /**
      * The Factory used to create EntityManagers
      */
@@ -46,48 +52,66 @@ public class WebDavRoot implements WebDavResource {
 
     @Override
     public javax.ws.rs.core.Response get() {
-        return null;
+        return javax.ws.rs.core.Response.status(404).build();
     }
 
     @Override
     public javax.ws.rs.core.Response put(@Context UriInfo uriInfo, InputStream entityStream, @HeaderParam(CONTENT_LENGTH) long contentLength) throws IOException, URISyntaxException {
-        return null;
+        return javax.ws.rs.core.Response.status(404).build();
     }
 
     @Override
     public javax.ws.rs.core.Response mkcol() {
-        return null;
+        return javax.ws.rs.core.Response.status(404).build();
     }
 
     @Override
     @Produces("application/xml")
     @PROPFIND
-    public javax.ws.rs.core.Response propfind(@Context UriInfo uriInfo, @HeaderParam(DEPTH) int depth, InputStream entityStream, @HeaderParam(CONTENT_LENGTH) long contentLength, @Context Providers providers, @Context HttpHeaders httpHeaders) throws URISyntaxException, IOException {
+    public javax.ws.rs.core.Response propfind(@Context final UriInfo uriInfo, @HeaderParam(DEPTH) final int depth,
+                                              final InputStream entityStream, @HeaderParam(CONTENT_LENGTH) final long contentLength,
+                                              @Context final Providers providers, @Context final HttpHeaders httpHeaders) throws URISyntaxException, IOException {
         try {
-
-            final EntityManager entityManager = getEntityManager(false);
-            final List<Topic> topics = entityManager.createQuery("SELECT topic FROM Topic").getResultList();
-            final List<Response> responses = new ArrayList<Response>();
-            for (final Topic topic : topics) {
-
-                final HRef hRef = new HRef(uriInfo.getRequestUriBuilder().path(topic.getId().toString()).build());
-                final CreationDate creationDate = new CreationDate(topic.getTopicTimeStamp());
-                final GetLastModified getLastModified = new GetLastModified(topic.getLastModifiedDate());
-                final GetContentType getContentType = new GetContentType("application/octet-stream");
-                final GetContentLength getContentLength = new GetContentLength(topic.getTopicXML().length());
-                final DisplayName displayName = new DisplayName(topic.getId().toString());
-                final Prop prop = new Prop(creationDate, getLastModified, getContentType, getContentLength, displayName);
+            if (depth == 0) {
+                /* A depth of zero means we are returning information about this item only */
+                final URI uri = uriInfo.getRequestUri();
+                final HRef hRef = new HRef(uri);
+                final Date lastModified = new Date();
+                final CreationDate creationDate = new CreationDate(lastModified);
+                final GetLastModified getLastModified = new GetLastModified(lastModified);
                 final Status status = new Status((StatusType) OK);
+                final Prop prop = new Prop(creationDate,getLastModified, COLLECTION);
                 final PropStat propStat = new PropStat(prop, status);
 
-                final Response davFile = new Response(hRef, null, null, null, propStat);
+                final Response folder = new Response(hRef, null, null, null, propStat);
 
-                responses.add(davFile);
+                return javax.ws.rs.core.Response.status(207).entity(new MultiStatus(folder)).type(XML_MIME).build();
+            } else {
+                /* Otherwise we are retuning info on the children in this collection */
+                final EntityManager entityManager = getEntityManager(false);
+                final List<Topic> topics = entityManager.createQuery("SELECT topic FROM Topic").getResultList();
+                final List<Response> responses = new ArrayList<Response>();
+                for (final Topic topic : topics) {
+
+                    final HRef hRef = new HRef(uriInfo.getRequestUriBuilder().path(topic.getId().toString()).build());
+                    final CreationDate creationDate = new CreationDate(topic.getTopicTimeStamp());
+                    final GetLastModified getLastModified = new GetLastModified(topic.getLastModifiedDate());
+                    final GetContentType getContentType = new GetContentType(OCTET_STREAM_MIME);
+                    final GetContentLength getContentLength = new GetContentLength(topic.getTopicXML().length());
+                    final DisplayName displayName = new DisplayName(topic.getId().toString());
+                    final Prop prop = new Prop(creationDate, getLastModified, getContentType, getContentLength, displayName);
+                    final Status status = new Status((StatusType) OK);
+                    final PropStat propStat = new PropStat(prop, status);
+
+                    final Response davFile = new Response(hRef, null, null, null, propStat);
+
+                    responses.add(davFile);
+                }
+
+                final MultiStatus st = new MultiStatus(responses.toArray(new Response[responses.size()]));
+
+                return javax.ws.rs.core.Response.status(207).entity(st).type(XML_MIME).build();
             }
-
-            final MultiStatus st = new MultiStatus(responses.toArray(new Response[responses.size()]));
-
-            return javax.ws.rs.core.Response.status(207).entity(st).type("application/xml;charset=UTF-8").build();
 
         } catch (final Exception ex) {
             return javax.ws.rs.core.Response.status(500).build();
@@ -96,27 +120,27 @@ public class WebDavRoot implements WebDavResource {
 
     @Override
     public javax.ws.rs.core.Response proppatch() {
-        return null;
+        return javax.ws.rs.core.Response.status(404).build();
     }
 
     @Override
     public javax.ws.rs.core.Response copy() {
-        return null;
+        return javax.ws.rs.core.Response.status(404).build();
     }
 
     @Override
     public javax.ws.rs.core.Response move(@Context UriInfo uriInfo, @HeaderParam(OVERWRITE) String overwriteStr, @HeaderParam(DESTINATION) String destination) throws URISyntaxException {
-        return null;
+        return javax.ws.rs.core.Response.status(404).build();
     }
 
     @Override
     public javax.ws.rs.core.Response delete() {
-        return null;
+        return javax.ws.rs.core.Response.status(404).build();
     }
 
     @Override
     public Object findResource(@PathParam("resource") String res) {
-        return null;
+        return javax.ws.rs.core.Response.status(404).build();
     }
 
     @Override
