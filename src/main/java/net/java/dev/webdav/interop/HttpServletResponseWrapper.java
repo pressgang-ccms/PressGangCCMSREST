28320a56a06e086e92030966532be968cbc2a33c
Matthew Casperson <matthewcasperson@gmail.com>
2013-05-23 15:37:48 +1000

    Added WebDAV interop filter, and fixed propfind responses of depth 1 to return info on the directory *and* its children.

---
+++ src/main/java/net/java/dev/webdav/interop/HttpServletResponseWrapper.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2008, 2009 Daniel MANZKE
+ * Copyright 2011 Markus KARG
+ *
+ * This file is part of webdav-interop.
+ *
+ * webdav-interop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * webdav-interop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webdav-interop.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.java.dev.webdav.interop;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This class wraps a real HttpServletResponse and wraps the methods which are
+ * responsible for setting the status code. It also catchs the OutputStream so
+ * it could not be closed and send to the client.
+ * 
+ * @author Daniel MANZKE (daniel.manzke@googlemail.com)
+ * @author Markus KARG (mkarg@java.net)
+ */
+public class HttpServletResponseWrapper extends
+		javax.servlet.http.HttpServletResponseWrapper {
+	private final ByteArrayOutputStream output;
+
+	public HttpServletResponseWrapper(HttpServletResponse response, int bufferSize) {
+		super(response);
+		output = new ByteArrayOutputStream(bufferSize);
+	}
+
+	public ServletOutputStream getOutputStream() {
+		return new FilterServletStream(output);
+	}
+
+	public PrintWriter getWriter() {
+		return new PrintWriter(getOutputStream(), true);
+	}
+	
+	public byte[] getByteArray() {
+		return output.toByteArray();
+	}
+	
+	private int statusCode;
+	
+	/*
+	 * Until this code is migrated to Servlet API 3.0 (where getStatusCode() is
+	 * already implemented in javax.servlet.http.HttpServletResponseWrapper) we
+	 * need to implemented this functionality on our own.
+	 */
+	public final int getStatusCode() {
+		return this.statusCode;
+	}
+
+	@Override
+	public final void sendError(final int statusCode, final String statusMessage) throws IOException {
+		super.sendError(statusCode, statusMessage);
+		this.statusCode = statusCode;
+	}
+
+	@Override
+	public final void sendError(final int statusCode) throws IOException {
+		super.sendError(statusCode);
+		this.statusCode = statusCode;
+	}
+
+	@Override
+	public final void setStatus(final int statusCode, final String statusMessage) {
+		super.setStatus(statusCode, statusMessage);
+		this.statusCode = statusCode;		
+	}
+
+	@Override
+	public final void setStatus(final int statusCode) {
+		super.setStatus(statusCode);
+		this.statusCode = statusCode;
+	}
+	
+}
---
+++ src/main/java/net/java/dev/webdav/interop/WindowsRedirectorPatchResource.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2008, 2009 Daniel MANZKE
+ * Copyright 2011 Markus KARG
+ *
+ * This file is part of webdav-interop.
+ *
+ * webdav-interop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * webdav-interop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webdav-interop.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.java.dev.webdav.interop;
+
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import static net.java.dev.webdav.interop.WindowsRedirectorPatchResourceFilter.*;
+
+/**
+ * This class represents a JAX-RS resource with only the OPTIONS-Method which 
+ * returns WebDAV-specific headers for the root resource.
+ * Why is this needed?
+ * 
+ * If you want to register a network folder. For example "net use X: http://mywebdavserver/filesystem".
+ * Microsoft MiniRedir expects that the root resource is webdav enabled.
+ * 
+ * If you add this Resource, your server will be webdav enabled on the root.
+ * 
+ * @author Daniel MANZKE (daniel.manzke@googlemail.com)
+ */
+@Path(ROOT_RESOURCE)
+public class WindowsRedirectorPatchResource {
+	
+	@OPTIONS
+	public javax.ws.rs.core.Response options() {
+		ResponseBuilder builder = javax.ws.rs.core.Response.noContent();
+		builder.header(DAV, "1");
+		/*
+		 * builder.header("Allow","");
+		 * OPTIONS, GET, HEAD, DELETE, PROPPATCH, COPY, MOVE, LOCK, UNLOCK, PROPFIND, PUT
+		 */
+		builder.header(MS_AUTHOR_VIA, DAV);
+		
+		return builder.build();
+	}
+}
