aa3239498a6136e2fc19fcdfb894ac7f044a18d2
Lee Newson <lnewson@redhat.com>
2013-09-06 00:24:14 +1000

    Moved the fixFailedContentSpec() method to the Content Spec commons library.

--- src/main/java/org/jboss/pressgang/ccms/server/utils/ContentSpecUtilities.java
+++ src/main/java/org/jboss/pressgang/ccms/server/utils/ContentSpecUtilities.java
@@ -6,7 +6,6 @@ import org.jboss.pressgang.ccms.contentspec.utils.CSTransformer;
 import org.jboss.pressgang.ccms.model.contentspec.ContentSpec;
 import org.jboss.pressgang.ccms.provider.ContentSpecProvider;
 import org.jboss.pressgang.ccms.provider.DBProviderFactory;
-import org.jboss.pressgang.ccms.utils.constants.CommonConstants;
 import org.jboss.pressgang.ccms.wrapper.ContentSpecWrapper;
 import org.jboss.resteasy.spi.InternalServerErrorException;
 import org.jboss.resteasy.spi.NotFoundException;
@@ -40,7 +39,7 @@ public class ContentSpecUtilities extends org.jboss.pressgang.ccms.contentspec.u
         }
 
         if (fix && ((ContentSpec) entity.unwrap()).getFailedContentSpec() != null) {
-            return fixFailedContentSpec(entityManager, (ContentSpec) entity.unwrap());
+            return fixFailedContentSpec(entity);
         } else {
             final CSTransformer transformer = new CSTransformer();
 
@@ -49,27 +48,7 @@ public class ContentSpecUtilities extends org.jboss.pressgang.ccms.contentspec.u
         }
     }
 
-    /**
-     * Fixes a failed Content Spec so that the ID and CHECKSUM are included. This is primarily an issue when creating new content specs
-     * and they are initially invalid.
-     *
-     * @param contentSpec The content spec to fix.
-     * @return The fixed failed content spec string.
-     */
-    public static String fixFailedContentSpec(final EntityManager entityManager, final ContentSpec contentSpec) {
-        if (contentSpec.getFailedContentSpec() == null || contentSpec.getFailedContentSpec().isEmpty()) {
-            return null;
-        } else {
-            if (contentSpec.getId() == null || getContentSpecChecksum(contentSpec.getFailedContentSpec()) != null) {
-                return contentSpec.getFailedContentSpec();
-            } else {
-                String cleanContentSpec = removeChecksumAndId(contentSpec.getFailedContentSpec());
-                final String serverContentSpec = getContentSpecText(contentSpec.getId(),
-                        contentSpec.getRevision() == null ? null : contentSpec.getRevision().intValue(), entityManager, false);
-                final String checksum = getContentSpecChecksum(serverContentSpec);
-                return CommonConstants.CS_CHECKSUM_TITLE + "=" + checksum + "\n" + CommonConstants.CS_ID_TITLE + "=" +
-                        contentSpec.getId() + "\n" + cleanContentSpec;
-            }
-        }
+    public static final String fixFailedContentSpec(final ContentSpec contentSpec) {
+        return fixFailedContentSpec(contentSpec.getId(), contentSpec.getFailedContentSpec());
     }
 }
