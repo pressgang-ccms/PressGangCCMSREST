6582cc3ef45b8c0de99379d6368d026ba2550e00
Lee Newson <lnewson@redhat.com>
2013-09-26 13:11:22 +1000

    Fixed Interceptors and Request mappers to return the correct version and to only work on the v1 endpoints.

--- src/main/java/org/jboss/pressgang/ccms/server/rest/v1/mapper/NotFoundExceptionMapper.java
+++ src/main/java/org/jboss/pressgang/ccms/server/rest/v1/mapper/NotFoundExceptionMapper.java
@@ -1,7 +1,7 @@
 package org.jboss.pressgang.ccms.server.rest.v1.mapper;
 
 import org.jboss.pressgang.ccms.rest.v1.constants.RESTv1Constants;
-import org.jboss.pressgang.ccms.server.rest.interceptor.VersionHeaderInterceptor;
+import org.jboss.pressgang.ccms.server.rest.v1.interceptor.RESTv1VersionHeaderInterceptor;
 import org.jboss.pressgang.ccms.server.utils.Constants;
 import org.jboss.pressgang.ccms.utils.common.VersionUtilities;
 import org.jboss.resteasy.spi.NotFoundException;
@@ -18,7 +18,7 @@ public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundExceptio
         return Response.status(Response.Status.NOT_FOUND)
                 .entity(exception.getMessage() + "\n")
                 .header("Content-Type", MediaType.TEXT_PLAIN)
-                .header(RESTv1Constants.X_PRESSGANG_VERSION_HEADER, VersionUtilities.getAPIVersion(VersionHeaderInterceptor.class))
+                .header(RESTv1Constants.X_PRESSGANG_VERSION_HEADER, VersionUtilities.getAPIVersion(RESTv1VersionHeaderInterceptor.class))
                 .header(RESTv1Constants.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, Constants.CORS_ALLOW_ORIGIN_HEADER)
                 .header(RESTv1Constants.ACCESS_CONTROL_EXPOSE_HEADERS, RESTv1Constants.X_PRESSGANG_VERSION_HEADER)
                 .build();
--- src/main/java/org/jboss/pressgang/ccms/server/rest/v1/mapper/UnauthorizedExceptionMapper.java
+++ src/main/java/org/jboss/pressgang/ccms/server/rest/v1/mapper/UnauthorizedExceptionMapper.java
@@ -1,11 +1,10 @@
 package org.jboss.pressgang.ccms.server.rest.v1.mapper;
 
 import org.jboss.pressgang.ccms.rest.v1.constants.RESTv1Constants;
-import org.jboss.pressgang.ccms.server.rest.interceptor.VersionHeaderInterceptor;
+import org.jboss.pressgang.ccms.server.rest.v1.interceptor.RESTv1VersionHeaderInterceptor;
 import org.jboss.pressgang.ccms.server.utils.Constants;
 import org.jboss.pressgang.ccms.utils.common.VersionUtilities;
 import org.jboss.resteasy.spi.UnauthorizedException;
-import org.jboss.resteasy.spi.WriterException;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -19,7 +18,7 @@ public class UnauthorizedExceptionMapper  implements ExceptionMapper<Unauthorize
         return Response.status(Response.Status.UNAUTHORIZED)
                 .entity(exception.getMessage() + "\n")
                 .header("Content-Type", MediaType.TEXT_PLAIN)
-                .header(RESTv1Constants.X_PRESSGANG_VERSION_HEADER, VersionUtilities.getAPIVersion(VersionHeaderInterceptor.class))
+                .header(RESTv1Constants.X_PRESSGANG_VERSION_HEADER, VersionUtilities.getAPIVersion(RESTv1VersionHeaderInterceptor.class))
                 .header(RESTv1Constants.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, Constants.CORS_ALLOW_ORIGIN_HEADER)
                 .header(RESTv1Constants.ACCESS_CONTROL_EXPOSE_HEADERS, RESTv1Constants.X_PRESSGANG_VERSION_HEADER)
                 .build();
