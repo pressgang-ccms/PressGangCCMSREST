6857323a404d634a5326191fbf37cc60b85d7194
Matthew Casperson <matthewcasperson@gmail.com>
2013-10-21 19:34:33 +1000

    Working on adding keywords

--- src/main/java/org/jboss/pressgang/ccms/server/rest/v1/TopicV1Factory.java
+++ src/main/java/org/jboss/pressgang/ccms/server/rest/v1/TopicV1Factory.java
@@ -165,8 +165,6 @@ public class TopicV1Factory extends RESTDataObjectFactory<RESTTopicV1, Topic, RE
                     mlt.setStopWords(stopWordsSet);
                 }
 
-
-
                 mlt.setFieldNames(new String[]{Topic.TOPIC_SEARCH_TEXT_FIELD_NAME});
 
                 final ArrayList<String> keywords = new ArrayList<String>();
