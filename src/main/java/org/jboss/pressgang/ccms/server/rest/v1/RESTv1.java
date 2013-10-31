a109c8f6d84830e22b37c4619f6f3fb60ab0af32
Matthew Casperson <mcaspers@redhat.com>
2013-10-31 13:02:48 +1000

    Added endpoints to allow minhashes to be calculated and recalculated

--- src/main/java/org/jboss/pressgang/ccms/server/rest/v1/RESTv1.java
+++ src/main/java/org/jboss/pressgang/ccms/server/rest/v1/RESTv1.java
@@ -82,7 +82,7 @@ import org.jboss.pressgang.ccms.model.User;
 import org.jboss.pressgang.ccms.model.contentspec.CSNode;
 import org.jboss.pressgang.ccms.model.contentspec.ContentSpec;
 import org.jboss.pressgang.ccms.model.contentspec.TranslatedCSNode;
-import org.jboss.pressgang.ccms.model.contentspec.TranslatedContentSpec;;
+import org.jboss.pressgang.ccms.model.contentspec.TranslatedContentSpec;
 import org.jboss.pressgang.ccms.rest.v1.collections.RESTBlobConstantCollectionV1;
 import org.jboss.pressgang.ccms.rest.v1.collections.RESTCategoryCollectionV1;
 import org.jboss.pressgang.ccms.rest.v1.collections.RESTFileCollectionV1;
@@ -180,9 +180,29 @@ public class RESTv1 extends BaseRESTv1 implements RESTBaseInterfaceV1, RESTInter
 
     @Override
     public void recalculateMinHash() {
-        final List<Topic> topics = entityManager.createQuery(Topic.SELECT_ALL_QUERY).getResultList();
-        for (final Topic topic : topics) {
-            entityManager.persist(topic);
+        try {
+            // Start a Transaction
+            transactionManager.begin();
+
+            // Join the transaction we just started
+            entityManager.joinTransaction();
+
+            final List<Topic> topics = entityManager.createQuery(Topic.SELECT_ALL_QUERY).getResultList();
+            for (final Topic topic : topics) {
+                // Change the min hash so the entity manager will attempt to save the topic.
+                // The actual value will be set in @PreUpdate on the Topic class
+                topic.setMinHash(topic.getMinHash() == null ? 0 : topic.getMinHash() == 0 ? 1 : 0);
+                // Handle topics that have invalid titles.
+                if (topic.getTopicTitle() == null || topic.getTopicTitle().trim().isEmpty()) {
+                    topic.setTopicTitle("Placeholder");
+                }
+                entityManager.persist(topic);
+            }
+
+            entityManager.flush();
+            transactionManager.commit();
+        } catch (final Exception ex) {
+            throw new InternalServerErrorException(ex);
         }
     }
 
