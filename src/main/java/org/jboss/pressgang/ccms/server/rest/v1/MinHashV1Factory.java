76f5deca59d82c15ad8bdda1fd5d3046ba836f75
Matthew Casperson <mcaspers@redhat.com>
2013-11-01 09:01:19 +1000

    Working on generating min hash signatures

---
+++ src/main/java/org/jboss/pressgang/ccms/server/rest/v1/MinHashV1Factory.java
@@ -0,0 +1,62 @@
+package org.jboss.pressgang.ccms.server.rest.v1;
+
+
+import org.jboss.pressgang.ccms.model.MinHash;
+import org.jboss.pressgang.ccms.rest.v1.collections.RESTMinHashCollectionV1;
+import org.jboss.pressgang.ccms.rest.v1.collections.items.RESTMinHashCollectionItemV1;
+import org.jboss.pressgang.ccms.rest.v1.constants.RESTv1Constants;
+import org.jboss.pressgang.ccms.rest.v1.entities.RESTMinHashV1;
+import org.jboss.pressgang.ccms.rest.v1.entities.base.RESTBaseEntityV1;
+import org.jboss.pressgang.ccms.rest.v1.expansion.ExpandDataTrunk;
+import org.jboss.pressgang.ccms.server.rest.v1.base.RESTDataObjectCollectionFactory;
+import org.jboss.pressgang.ccms.server.rest.v1.base.RESTDataObjectFactory;
+import org.jboss.pressgang.ccms.server.utils.EnversUtilities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinHashV1Factory extends RESTDataObjectFactory<RESTMinHashV1, MinHash, RESTMinHashCollectionV1, RESTMinHashCollectionItemV1> {
+    @Override
+    public RESTMinHashV1 createRESTEntityFromDBEntityInternal(final MinHash entity, final String baseUrl, final String dataType,
+                                                              final ExpandDataTrunk expand, final Number revision, final boolean expandParentReferences) {
+        assert entity != null : "Parameter entity can not be null";
+        assert baseUrl != null : "Parameter baseUrl can not be null";
+
+        final RESTMinHashV1 retValue = new RESTMinHashV1();
+
+        retValue.setId(entity.getMinHashId());
+        retValue.setMinHashFuncId(entity.getMinHashFuncID());
+        retValue.setMinHash(entity.getMinHash());
+
+        final List<String> expandOptions = new ArrayList<String>();
+        if (revision == null) expandOptions.add(RESTBaseEntityV1.REVISIONS_NAME);
+
+        retValue.setExpand(expandOptions);
+
+        // REVISIONS
+        if (revision == null && expand != null && expand.contains(RESTBaseEntityV1.REVISIONS_NAME)) {
+            retValue.setRevisions(RESTDataObjectCollectionFactory.create(RESTMinHashCollectionV1.class, this, entity,
+                    EnversUtilities.getRevisions(entityManager, entity), RESTBaseEntityV1.REVISIONS_NAME, dataType, expand, baseUrl,
+                    entityManager));
+        }
+
+        retValue.setLinks(baseUrl, RESTv1Constants.MINHASH_URL_NAME, dataType, retValue.getId());
+
+        return retValue;
+    }
+
+    @Override
+    public void syncDBEntityWithRESTEntityFirstPass(final MinHash entity, final RESTMinHashV1 dataObject) {
+        // The generation of minhashes is done in the TopicV1Factory. These values can not be set by the client.
+    }
+
+    @Override
+    public void syncDBEntityWithRESTEntitySecondPass(final MinHash entity, final RESTMinHashV1 dataObject) {
+        // The generation of minhashes is done in the TopicV1Factory. These values can not be set by the client.
+    }
+
+    @Override
+    protected Class<MinHash> getDatabaseClass() {
+        return MinHash.class;
+    }
+}
