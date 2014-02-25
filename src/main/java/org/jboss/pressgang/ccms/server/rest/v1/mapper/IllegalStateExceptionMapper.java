59296cf621e786a94a141a1f4bbacc7e3fcf6b11
Matthew Casperson <mcaspers@redhat.com>
2014-02-25 15:23:29 +1000

    Added exception mapper for IllegalStateException

---
+++ src/main/java/org/jboss/pressgang/ccms/server/rest/v1/mapper/IllegalStateExceptionMapper.java
@@ -0,0 +1,11 @@
+package org.jboss.pressgang.ccms.server.rest.v1.mapper;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+public class IllegalStateExceptionMapper extends BaseExceptionMapper<IllegalStateException> implements ExceptionMapper<IllegalStateException> {
+    @Override
+    public Response toResponse(final IllegalStateException exception) {
+        return buildPlainTextResponse(Response.Status.INTERNAL_SERVER_ERROR, exception);
+    }
+}
