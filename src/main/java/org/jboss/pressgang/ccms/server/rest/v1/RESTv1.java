cffe8bd4233dfbc48dcfe9e87bd9e12fd469668d
Matthew Casperson <matthewcasperson@gmail.com>
2014-04-03 08:21:45 +1000

    Fixing https://bugzilla.redhat.com/show_bug.cgi?id=1067232

--- src/main/java/org/jboss/pressgang/ccms/server/rest/v1/RESTv1.java
+++ src/main/java/org/jboss/pressgang/ccms/server/rest/v1/RESTv1.java
@@ -347,7 +347,7 @@ public class RESTv1 extends BaseRESTv1 implements RESTBaseInterfaceV1, RESTInter
     private void recalculateMinHashes(final boolean missingOnly) {
         try {
             final String topicQuery =
-                "SELECT topic.topicId FROM Topic as Topic WHERE NOT (topic.topicXML IS NULL AND topic.topicXML <> '')" +
+                "SELECT topic.topicId FROM Topic as Topic WHERE NOT (topic.topicXML IS NULL AND length(trim(topic.topicXML)) <> 0)" +
                 (missingOnly ? " AND SIZE(topic.minHashes) != " + org.jboss.pressgang.ccms.model.constants.Constants
                 .NUM_MIN_HASHES : "");
 
