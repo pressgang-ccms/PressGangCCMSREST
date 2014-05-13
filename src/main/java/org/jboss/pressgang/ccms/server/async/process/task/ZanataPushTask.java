baecf915aae85906781adcde8065576c489167c1
Lee Newson <lnewson@redhat.com>
2014-05-14 09:00:42 +1000

    Fixed BZ#1095978 - Verbatim elements aren't being broken down correctly for translation.

--- src/main/java/org/jboss/pressgang/ccms/server/async/process/task/ZanataPushTask.java
+++ src/main/java/org/jboss/pressgang/ccms/server/async/process/task/ZanataPushTask.java
@@ -301,7 +301,7 @@ public class ZanataPushTask extends ProcessRESTTask<Boolean> {
             resource.setType(ResourceType.FILE);
 
             // Get the translatable nodes
-            final List<StringToNodeCollection> translatableStrings = DocBookUtilities.getTranslatableStringsV2(doc, false);
+            final List<StringToNodeCollection> translatableStrings = DocBookUtilities.getTranslatableStringsV3(doc, false);
 
             // Add the translatable nodes to the zanata document
             for (final StringToNodeCollection translatableStringData : translatableStrings) {
