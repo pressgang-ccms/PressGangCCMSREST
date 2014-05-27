920c54dfbd2960052c79c9fde321b47bda361cc3
Lee Newson <lnewson@redhat.com>
2014-05-27 12:56:44 +1000

    Removed the hard dependency on the schema for BZ#1101327 - Add messages to JMS queue when topics and specs are updated.

--- src/main/java/org/jboss/pressgang/ccms/server/messaging/UpdatedEntities.java
+++ src/main/java/org/jboss/pressgang/ccms/server/messaging/UpdatedEntities.java
@@ -1,33 +1,35 @@
 package org.jboss.pressgang.ccms.server.messaging;
 
-import org.hibernate.envers.AuditReader;
-import org.hibernate.envers.AuditReaderFactory;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.IllegalStateException;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Hashtable;
+import java.util.List;
+
 import org.jboss.pressgang.ccms.filter.utils.EntityUtilities;
 import org.jboss.pressgang.ccms.model.Topic;
 import org.jboss.pressgang.ccms.model.config.ApplicationConfig;
 import org.jboss.pressgang.ccms.model.contentspec.ContentSpec;
 import org.jboss.pressgang.ccms.server.utils.ResourceProducer;
 import org.jboss.pressgang.ccms.utils.common.CollectionUtilities;
-import org.joda.time.DateTime;
-import org.jppf.classloader.ResourceProvider;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.annotation.Resource;
-import javax.ejb.*;
-import javax.inject.Inject;
-import javax.jms.*;
-import javax.jms.IllegalStateException;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.persistence.EntityManager;
-import javax.persistence.NamedNativeQuery;
-import javax.persistence.PersistenceContext;
-import java.util.Date;
-import java.util.Hashtable;
-import java.util.List;
-import javax.ejb.Timer;
 
 /**
  * This EJB will poll the database periodically looking for changes that can be broadcast via the message
@@ -65,8 +67,6 @@ public class UpdatedEntities {
      */
     private static final String SPEC_UPDATE_QUEUE = "java:jboss/topics/updatedspec";
 
-    private static final String MAX_REV_QUERY = "SELECT MAX( REV ) FROM Skynet.REVINFO";
-
     /**
      * How many times to retry opening a connection and resending a message
      */
@@ -94,16 +94,17 @@ public class UpdatedEntities {
 
     @Timeout
     public void onTimeout(final Timer timer) {
-        checkForUpdatedTopics(timer);
-        checkForUpdatedSpecs(timer);
+        final Integer currentRevision = getCurrentRevision();
+        checkForUpdatedTopics(timer, currentRevision);
+        checkForUpdatedSpecs(timer, currentRevision);
         triggerNextTimeout(timer);
     }
 
-    private void checkForUpdatedTopics(final Timer timer) {
+    protected Integer getCurrentRevision() {
+        return (Integer) entityManager.createQuery("SELECT MAX(id) FROM LoggingRevisionEntity").getSingleResult();
+    }
 
-        final AuditReader reader = AuditReaderFactory.get(entityManager);
-        final Integer thisTopicUpdate = (Integer)entityManager.createNativeQuery(MAX_REV_QUERY).getSingleResult();
-
+    protected void checkForUpdatedTopics(final Timer timer, final Integer thisTopicUpdate) {
         if (lastTopicUpdate == null) {
             // getEditedEntitiesByRevision will get revisions between and inclusive of the supplied revisions.
             // we are only interested in revisions above the last largest one.
@@ -130,10 +131,7 @@ public class UpdatedEntities {
         }
     }
 
-    public void checkForUpdatedSpecs(final Timer timer) {
-        final AuditReader reader = AuditReaderFactory.get(entityManager);
-        final Integer thisSpecUpdate = (Integer)entityManager.createNativeQuery(MAX_REV_QUERY).getSingleResult();
-
+    public void checkForUpdatedSpecs(final Timer timer, final Integer thisSpecUpdate) {
         if (lastSpecUpdate == null) {
             lastSpecUpdate = thisSpecUpdate + 1;
         } else {
