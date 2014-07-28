4508236e3ff101fa6a676cf2c2914054f34eed5e
Matthew Casperson <matthewcasperson@gmail.com>
2014-07-28 14:07:07 +1000

    Added license info for LGPL v3

--- src/main/java/net/java/dev/webdav/jaxrs/xml/elements/MultiStatus.java
+++ src/main/java/net/java/dev/webdav/jaxrs/xml/elements/MultiStatus.java
@@ -1,21 +1,21 @@
 /*
- * Copyright 2008, 2009, 2011 Markus KARG
- *
- * This file is part of webdav-jaxrs.
- *
- * webdav-jaxrs is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * webdav-jaxrs is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with webdav-jaxrs.  If not, see <http://www.gnu.org/licenses/>.
- */
+  Copyright 2011-2014 Red Hat
+
+  This file is part of PresGang CCMS.
+
+  PresGang CCMS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  PresGang CCMS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with PresGang CCMS.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 package net.java.dev.webdav.jaxrs.xml.elements;
 
--- src/main/java/org/jboss/pressgang/ccms/server/webdav/managers/ResourceTypes.java
+++ src/main/java/org/jboss/pressgang/ccms/server/webdav/managers/ResourceTypes.java
@@ -1,3 +1,22 @@
+/*
+  Copyright 2011-2014 Red Hat
+
+  This file is part of PresGang CCMS.
+
+  PresGang CCMS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  PresGang CCMS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with PresGang CCMS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package org.jboss.pressgang.ccms.server.webdav.managers;
 
 /**
